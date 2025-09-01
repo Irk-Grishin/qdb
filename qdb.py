5d3b12212f2dabdc0511193a4a18c58799677f03
Alex_G <irk.grishin@gmail.com>
2025-09-02 02:28:22 +0300

    Initial commit: QDB project structure

---
+++ qdb.py
@@ -0,0 +1,7 @@
+class QDB:
+    def __init__(self):
+        pass
+
+    def query(self, sql: str):
+        return f"Executing '{sql}' across the universe..."
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="qdb",
+    version="0.0.1",
+    py_modules=["qdb"],
+    install_requires=[],
+)
+
